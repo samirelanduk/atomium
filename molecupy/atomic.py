b7f5e8f2c1a36a30a1abb3771e692c6aea6ff05b
Sam Ireland <sam.ireland.uk@gmail.com>
2016-05-04 00:31:30 +0100

    Refactor Atom validation layout

--- molecupy/atomic.py
+++ molecupy/atomic.py
@@ -5,28 +5,25 @@ class Atom:
     def __init__(self, x, y, z, element, atom_id=None, atom_name=None):
         for coordinate in (x, y, z):
             if not isinstance(coordinate, float):
-                error_message = "'%s' is not a valid coordinate" % str(coordinate)
-                raise TypeError(error_message)
-        if not isinstance(element, str):
-            error_message = "'%s' is not a valid element" % str(element)
-            raise TypeError(error_message)
-        if len(element) == 0:
-            error_message = "An Atom's element cannot be an empty string"
-            raise InvalidElementError(error_message)
-        elif len(element) >= 2:
-            error_message = "'%s' is not a valid element" % element
-            raise InvalidElementError(error_message)
-        if not isinstance(atom_id, int) and atom_id is not None:
-            error_message = "'%s' is not a valid atom_id" % str(atom_id)
-            raise TypeError(error_message)
-        if not isinstance(atom_name, str) and atom_name is not None:
-            error_message = "'%s' is not a valid atom_name" % str(atom_name)
-            raise TypeError(error_message)
+                raise TypeError("'%s' is not a valid coordinate" % str(coordinate))
         self.x = x
         self.y = y
         self.z = z
+
+        if not isinstance(element, str):
+            raise TypeError("'%s' is not a valid element" % str(element))
+        if len(element) == 0:
+            raise InvalidElementError("Atom's element can't be an empty string")
+        elif len(element) >= 2:
+            raise InvalidElementError("'%s' is not a valid element" % element)
         self.element = element
+
+        if not isinstance(atom_id, int) and atom_id is not None:
+            raise TypeError("'%s' is not a valid atom_id" % str(atom_id))
         self.atom_id = atom_id
+        
+        if not isinstance(atom_name, str) and atom_name is not None:
+            raise TypeError("'%s' is not a valid atom_name" % str(atom_name))
         self.atom_name = atom_name
 
 

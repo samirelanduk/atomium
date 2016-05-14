61c2858b4872aa414941603c136c7abba7b0f006
samirelanduk <sam.ireland.uk@gmail.com>
2016-05-14 16:25:13 +0100

    Add Pdb atributes

--- molecupy/parsers/pdb/pdb.py
+++ molecupy/parsers/pdb/pdb.py
@@ -2,3 +2,30 @@ class Pdb:
 
     def __init__(self, data_file):
         self.data_file = data_file
+
+        transfer_attrs = [
+         "classification",
+         "deposition_date",
+         "pdb_code",
+         "is_obsolete",
+         "obsolete_date",
+         "replacement_code",
+         "title",
+         "split_codes",
+         "caveat",
+         "keywords",
+         "experimental_techniques",
+         "model_num",
+         "model_annotations",
+         "authors",
+         "revisions",
+         "supercedes",
+         "supercede_date",
+         "journal"
+        ]
+        for attr in transfer_attrs:
+            self.__dict__[attr] = self.data_file.__dict__[attr]
+
+
+    def __repr__(self):
+        return "<Pdb (%s)>" % (self.pdb_code if self.pdb_code else "????")
--- tests/parsers/pdb/test_pdb.py
+++ tests/parsers/pdb/test_pdb.py
@@ -17,6 +17,15 @@ class PdbTest(TestCase):
     def check_valid_pdb(self, pdb):
         self.assertIsInstance(pdb, Pdb)
         self.assertIsInstance(pdb.data_file, PdbDataFile)
+        self.assertRegex(
+         str(pdb),
+         r"<Pdb \(([^\s]{4})\)>"
+        )
+
+
+    def check_property(self, property_, type_):
+        if property_ is not None:
+            self.assertIsInstance(property_, type_)
 
 
 
@@ -25,3 +34,26 @@ class PdbCreationTests(PdbTest):
     def test_can_make_pdb(self):
         self.check_valid_pdb(self.pdb)
         self.check_valid_pdb(self.empty_pdb)
+
+
+
+class PdbPropertiesTest(PdbTest):
+
+    def test_pdb_has_properties(self):
+        self.check_property(self.pdb.classification, str)
+        self.check_property(self.pdb.deposition_date, datetime.date)
+        self.check_property(self.pdb.pdb_code, str)
+        self.check_property(self.pdb.is_obsolete, bool)
+        self.check_property(self.pdb.obsolete_date, datetime.date)
+        self.check_property(self.pdb.replacement_code, str)
+        self.check_property(self.pdb.title, str)
+        self.check_property(self.pdb.split_codes, list)
+        self.check_property(self.pdb.caveat, str)
+        self.check_property(self.pdb.keywords, list)
+        self.check_property(self.pdb.experimental_techniques, list)
+        self.check_property(self.pdb.model_num, int)
+        self.check_property(self.pdb.model_annotations, list)
+        self.check_property(self.pdb.revisions, list)
+        self.check_property(self.pdb.supercedes, list)
+        self.check_property(self.pdb.supercede_date, datetime.date)
+        self.check_property(self.pdb.journal, dict)

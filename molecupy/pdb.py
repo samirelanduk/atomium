e51a40b85b3080c0657717c725caca9b92b700ac
samirelanduk <sam.ireland.uk@gmail.com>
2016-05-15 20:04:26 +0100

    pdb properties transfer

--- molecupy/pdb.py
+++ molecupy/pdb.py
@@ -3,6 +3,29 @@ class Pdb:
     def __init__(self, data_file):
         self.data_file = data_file
 
+        transfer_attrs = [
+         "classification",
+         "deposition_date",
+         "pdb_code",
+         "is_obsolete",
+         "obsolete_date",
+         "replacement_code",
+         "title",
+         "split_codes",
+         "caveat",
+         "keywords",
+         "experimental_techniques",
+         "model_count",
+         "model_annotations",
+         "authors",
+         "revisions",
+         "supercedes",
+         "supercede_date",
+         "journal"
+        ]
+        for attr in transfer_attrs:
+            self.__dict__[attr] = self.data_file.__dict__[attr]
+
 
     def __repr__(self):
-        return "<Pdb (%s)>" % ("????")
+        return "<Pdb (%s)>" % (self.pdb_code if self.pdb_code else "????")

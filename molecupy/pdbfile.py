02a4be2d19e428e1e249c6c0522eecb7eeb9904c
Sam Ireland <sam.ireland.uk@gmail.com>
2016-05-15 01:02:50 +0100

    Add PdbFile

--- molecupy/pdbfile.py
+++ molecupy/pdbfile.py
@@ -28,3 +28,30 @@ class PdbRecord:
     def get_as_string(self, start, end):
         splice = self[start:end]
         return str(splice) if splice is not None else None
+
+
+
+class PdbFile:
+
+    def __init__(self, file_string):
+        self.file_contents = "".join([
+         char for char in file_string if 32 <= ord(char) <= 126 or char=="\n"
+        ])
+        self.records = [
+         PdbRecord(line, i) for i, line in
+          enumerate(self.file_contents.split("\n"), start=1) if line
+        ]
+
+
+    def __repr__(self):
+        return "<PdbFile (%i records)>" % len(self.records)
+
+
+    def get_record_by_name(self, record_name):
+        for record in self.records:
+            if record.name == record_name:
+                return record
+
+
+    def get_records_by_name(self, record_name):
+        return [record for record in self.records if record.name == record_name]

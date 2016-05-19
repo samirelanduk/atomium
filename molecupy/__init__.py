822ed448fa1e2d0b805ff090d9d8ae2370f7c60a
Sam Ireland <sam.ireland.uk@gmail.com>
2016-05-19 19:00:04 +0100

    Update version to 0.2

--- molecupy/__init__.py
+++ molecupy/__init__.py
@@ -1,4 +1,4 @@
 from .access import get_pdb_remotely, get_pdb_from_file, pdb_from_string
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Sam Ireland"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
  name="molecupy",
- version="0.1.0",
+ version="0.2.0",
  description="A Python PDB parser.",
  url="https://molecupy.readthedocs.org",
  author="Sam Ireland",

20ffa916d61d390a21483c3421e6265da9e17a54
Sam Ireland <sam.ireland.uk@gmail.com>
2016-07-12 15:33:40 +0100

    Add element length check

--- molecupy/structures.py
+++ molecupy/structures.py
@@ -3,6 +3,8 @@ class Atom:
     def __init__(self, element, atom_id, atom_name):
         if not isinstance(element, str):
             raise TypeError("element must be str, not '%s'" % str(element))
+        if not 0 < len(element) <= 2:
+            raise ValueError("element must be of length 1 or 2, not %s" % element)
         self._element = element
         if not isinstance(atom_id, int):
             raise TypeError("atom_id must be int, not '%s'" % str(atom_id))
--- tests/test_atoms.py
+++ tests/test_atoms.py
@@ -20,6 +20,15 @@ class AtomCreationTests(TestCase):
             atom = Atom(9, 100, "CA")
 
 
+    def test_element_must_be_correct_length(self):
+        with self.assertRaises(ValueError):
+            atom = Atom("", 100, "CA")
+        with self.assertRaises(ValueError):
+            atom = Atom("CAC", 100, "CA")
+        atom = Atom("MG", 100, "CA")
+        atom = Atom("M", 100, "CA")
+
+
     def test_atom_id_must_be_int(self):
         with self.assertRaises(TypeError):
             atom = Atom("C", "100", "CA")

f384c4c9424b64f345d7be77260c3b3248d1b66c
Sam Ireland <sam.ireland.uk@gmail.com>
2016-07-23 17:09:00 +0100

    Add chain repr and ID validation

--- molecupy/structures/chains.py
+++ molecupy/structures/chains.py
@@ -111,8 +111,14 @@ class ResiduicSequence(ResiduicStructure):
 class Chain(ResiduicSequence):
 
     def __init__(self, chain_id, *residues):
+        if not isinstance(chain_id, str):
+            raise TypeError("'%s' is not a valid chain_id" % str(chain_id))
         self._chain_id = chain_id
         ResiduicSequence.__init__(self, *residues)
+
+
+    def __repr__(self):
+        return "<Chain %s (%i residues)>" % (self._chain_id, len(self._residues))
 
 
 
--- tests/test_chains.py
+++ tests/test_chains.py
@@ -11,8 +11,18 @@ class ChainTest(TestCase):
 
 class ChainCreationTests(ChainTest):
 
-    def test_can_create_small_molecule(self):
+    def test_can_create_chain(self):
         chain = Chain("A", *self.residues)
         self.assertIsInstance(chain, ResiduicSequence)
         self.assertEqual(chain._chain_id, "A")
         self.assertEqual(chain._residues, self.residues)
+
+
+    def test_chain_id_must_be_str(self):
+        with self.assertRaises(TypeError):
+            Chain(200, *self.residues)
+
+
+    def test_chain_repr(self):
+        chain = Chain("A", *self.residues)
+        self.assertEqual(str(chain), "<Chain A (10 residues)>")

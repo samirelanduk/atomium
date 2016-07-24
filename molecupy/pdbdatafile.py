8ad4df263af86aef4188b43fa97961ac972423ec
Sam Ireland <sam.ireland.uk@gmail.com>
2016-07-24 18:31:59 +0100

    Add record merging method

--- molecupy/pdbdatafile.py
+++ molecupy/pdbdatafile.py
@@ -19,3 +19,13 @@ def date_from_string(s):
     return datetime.datetime.strptime(
      s, "%d-%b-%y"
     ).date()
+
+
+def merge_records(records, start, join=" ", dont_condense=""):
+    string = join.join(
+     str(record[start:]
+    ) if record[start:] else "" for record in records)
+    condense = [char for char in " ;:,-" if char not in dont_condense]
+    for char in condense:
+        string = string.replace(char + " ", char)
+    return string
--- tests/test_pdbdatafiles.py
+++ tests/test_pdbdatafiles.py
@@ -1,7 +1,7 @@
 import datetime
 from unittest import TestCase
 from molecupy.pdbfile import PdbFile, PdbRecord
-from molecupy.pdbdatafile import PdbDataFile, date_from_string
+from molecupy.pdbdatafile import PdbDataFile, date_from_string, merge_records
 
 class PdbDataFileTest(TestCase):
 
@@ -31,4 +31,69 @@ class DateFromStringTests(TestCase):
         self.assertEqual(
          date_from_string("28-SEP-99"),
          datetime.datetime(1999, 9, 28).date()
+        )
+
+
+
+class RecordMergingTests(TestCase):
+
+    def setUp(self):
+        self.records = [PdbRecord(l, 1) for l in [
+         "0123456789",
+         "abcdefghij",
+         "0123456789"
+        ]]
+
+        self.punc_records = [PdbRecord(l, 1) for l in [
+         "0123, 456789",
+         "abcd  efghij",
+         "0123; 456789"
+        ]]
+
+
+    def test_can_merge_records(self):
+        self.assertEqual(
+         merge_records(self.records, 5),
+         "56789 fghij 56789"
+        )
+        self.assertEqual(
+         merge_records(self.records, 8),
+         "89 ij 89"
+        )
+
+
+    def test_can_vary_join(self):
+        self.assertEqual(
+         merge_records(self.records, 5, join=""),
+         "56789fghij56789"
+        )
+        self.assertEqual(
+         merge_records(self.records, 8, join="."),
+         "89.ij.89"
+        )
+
+
+    def test_can_condense(self):
+        self.assertEqual(
+         merge_records(self.punc_records, 2),
+         "23,456789 cd efghij 23;456789"
+        )
+
+
+    def test_can_ignore_consensors(self):
+        self.assertEqual(
+         merge_records(self.punc_records, 2, dont_condense=","),
+         "23, 456789 cd efghij 23;456789"
+        )
+        self.assertEqual(
+         merge_records(self.punc_records, 2, dont_condense=";"),
+         "23,456789 cd efghij 23; 456789"
+        )
+        self.assertEqual(
+         merge_records(self.punc_records, 2, dont_condense=";,"),
+         "23, 456789 cd efghij 23; 456789"
+        )
+        self.assertEqual(
+         merge_records(self.punc_records, 2, dont_condense=";, "),
+         "23, 456789 cd  efghij 23; 456789"
         )

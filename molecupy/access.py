24a537122a20f54aeb47d3b967308474f801c76c
Sam Ireland <sam.ireland.uk@gmail.com>
2016-07-26 11:29:59 +0100

    Add File opener method

--- molecupy/access.py
+++ molecupy/access.py
@@ -4,3 +4,8 @@ from .pdb import Pdb
 
 def pdb_from_string(text):
     return Pdb(PdbDataFile(PdbFile(text)))
+
+
+def get_pdb_from_file(path):
+    with open(path) as f:
+        return pdb_from_string(f.read())
--- tests/test_access.py
+++ tests/test_access.py
@@ -1,5 +1,6 @@
+import os
 from unittest import TestCase
-from molecupy.access import pdb_from_string
+from molecupy.access import pdb_from_string, get_pdb_from_file
 from molecupy.pdb import Pdb
 
 class PdbFromStringTests(TestCase):
@@ -7,5 +8,14 @@ class PdbFromStringTests(TestCase):
     def test_can_make_pdb_from_string(self):
         text = "KEYWDS    TIM BARREL, LYASE"
         pdb = pdb_from_string(text)
+        self.assertIsInstance(pdb, Pdb)
+        self.assertEqual(pdb.keywords(), ["TIM BARREL", "LYASE"])
+
+
+
+class PdbFromFileTests(TestCase):
+
+    def test_can_make_pdb_from_file(self):
+        pdb = get_pdb_from_file(os.path.sep.join(["tests", "1LOL.pdb"]))
         self.assertIsInstance(pdb, Pdb)
         self.assertEqual(pdb.keywords(), ["TIM BARREL", "LYASE"])

f2b2f69eeeec3c19d9c15a837235c0e8386da0f8
Sam Ireland <sam.ireland.uk@gmail.com>
2016-07-31 22:23:33 +0100

    Readd access.py docs

--- molecupy/access.py
+++ molecupy/access.py
@@ -1,3 +1,7 @@
+"""This module contains the functions used to access PDB files themselves. These
+are the only functions to be imported into the top level directory, and so are
+all accesisble by importing molecupy itself."""
+
 import requests
 from .pdbfile import PdbFile
 from .pdbdatafile import PdbDataFile
@@ -5,15 +9,29 @@ from .pdb import Pdb
 from .exceptions import InvalidPdbCodeError
 
 def pdb_from_string(text):
+     """Creates a :py:class:`.Pdb` object from the text of a PDB file.
+    :param str string: The raw text of a PDB file.
+    :rtype: :py:class:`.Pdb`"""
+
     return Pdb(PdbDataFile(PdbFile(text)))
 
 
 def get_pdb_from_file(path):
+    """Gets a :py:class:`.Pdb` object from a PDB file stored on disk.
+    :param str path: The path to the PDB file.
+    :rtype: :py:class:`.Pdb`"""
+
     with open(path) as f:
         return pdb_from_string(f.read())
 
 
 def get_pdb_remotely(code):
+    """Gets a :py:class:`.Pdb` object from a PDB code.
+    The file is requested from the RCSB servers via a HTTP request.
+    :param str code: The PDB code required - e.g. '1NVQ'.
+    :rtype: :py:class:`.Pdb`
+    :raises: :class:`.InvalidPdbCodeError` if the PDB doesn't exist"""
+    
     response = requests.get(
      "http://www.ebi.ac.uk/pdbe/entry-files/pdb%s.ent" % code.lower()
     )

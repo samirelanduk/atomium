432a7765946c3d13761075316cbdbd43c351cc8f
Sam Ireland <sam.ireland.uk@gmail.com>
2016-11-03 14:39:53 +0000

    Re-add tests which don't look at PdbRecord number

--- tests/pdb_tests/test_pdb_records.py
+++ tests/pdb_tests/test_pdb_records.py
@@ -28,11 +28,10 @@ class RecordCreationTests(TestCase):
 
 
 
-'''class RecordPropertyTests(TestCase):
+class RecordPropertyTests(TestCase):
 
     def test_record_properties(self):
-        record = PdbRecord("TEST   123  123.8    HYT", 23)
-        self.assertEqual(record._number, record.number())
+        record = PdbRecord("TEST   123  123.8    HYT")
         self.assertEqual(record._name, record.name())
         self.assertEqual(record._text, record.text())
         self.assertTrue(record._content, record.content())
@@ -42,45 +41,38 @@ class RecordCreationTests(TestCase):
 class RecordAccessTests(TestCase):
 
     def setUp(self):
-        self.line = "TEST   123  123.8    HYT"
+        self.record = PdbRecord("TEST   123  123.8    HYT")
 
 
     def test_can_get_individual_characters(self):
-        record = PdbRecord(self.line, 23)
-        self.assertEqual(record[0], "T")
-        self.assertEqual(record[21], "H")
+        self.assertEqual(self.record[0], "T")
+        self.assertEqual(self.record[21], "H")
 
 
     def test_can_get_strings_from_record(self):
-        record = PdbRecord(self.line, 23)
-        self.assertEqual(record[1:4], "EST")
-        self.assertEqual(record[21:24], "HYT")
+        self.assertEqual(self.record[1:4], "EST")
+        self.assertEqual(self.record[21:24], "HYT")
 
 
     def test_record_indexes_will_strip_strings(self):
-        record = PdbRecord(self.line, 23)
-        self.assertEqual(record[0:7], "TEST")
-        self.assertEqual(record[19:24], "HYT")
-        self.assertEqual(record[19:34], "HYT")
+        self.assertEqual(self.record[0:7], "TEST")
+        self.assertEqual(self.record[19:24], "HYT")
+        self.assertEqual(self.record[19:34], "HYT")
 
 
     def test_records_can_covert_to_int(self):
-        record = PdbRecord(self.line, 23)
-        self.assertEqual(record[5:11], 123)
+        self.assertEqual(self.record[5:11], 123)
 
 
     def test_records_can_covert_to_float(self):
-        record = PdbRecord(self.line, 23)
-        self.assertEqual(record[10:19], 123.8)
+        self.assertEqual(self.record[10:19], 123.8)
 
 
     def test_can_force_record_to_return_string(self):
-        record = PdbRecord(self.line, 23)
-        self.assertEqual(record.get_as_string(5, 11), "123")
-        self.assertEqual(record.get_as_string(10, 19), "123.8")
+        self.assertEqual(self.record.get_as_string(5, 11), "123")
+        self.assertEqual(self.record.get_as_string(10, 19), "123.8")
 
 
     def test_empty_sections_return_none(self):
-        record = PdbRecord(self.line, 23)
-        self.assertIs(record[17:21], None)
-        self.assertIs(record.get_as_string(17, 21), None)'''
+        self.assertIs(self.record[17:21], None)
+        self.assertIs(self.record.get_as_string(17, 21), None)

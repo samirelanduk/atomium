485243cf358dcc4a958a3911925b762ab6be14f2
Sam Ireland <sam.ireland.uk@gmail.com>
2016-11-08 12:58:52 +0000

    Make some name changes in bond tests

--- tests/structure_tests/test_bonds.py
+++ tests/structure_tests/test_bonds.py
@@ -1,15 +1,15 @@
 from unittest import TestCase
 import unittest.mock
-from molecupy.structures import Bond, PdbAtom
+from molecupy.structures import Bond, Atom
 from molecupy.exceptions import LongBondWarning
 
 class BondTest(TestCase):
 
     def setUp(self):
-        self.atom1 = unittest.mock.Mock(spec=PdbAtom)
+        self.atom1 = unittest.mock.Mock(Atom)
         self.atom1.atom_id.return_value = 100
         self.atom1.bonds.return_value = set()
-        self.atom2 = unittest.mock.Mock(spec=PdbAtom)
+        self.atom2 = unittest.mock.Mock(Atom)
         self.atom2.atom_id.return_value = 101
         self.atom2.bonds.return_value = set()
         self.atom1.distance_to.return_value = 2.1
@@ -24,7 +24,7 @@ class BondCreationTests(BondTest):
         self.assertEqual(bond._atoms, set((self.atom1, self.atom2)))
 
 
-    def test_bond_requires_pdb_atoms(self):
+    def test_bond_requires_atoms(self):
         with self.assertRaises(TypeError):
             bond = Bond(self.atom1, "atom2")
 
@@ -56,7 +56,7 @@ class BondPropertyTests(BondTest):
         self.assertEqual(bond.atoms(), set((self.atom1, self.atom2)))
 
 
-    def test_atoms_not_modifiable(self):
+    def test_atoms_not_directly_modifiable(self):
         bond = Bond(self.atom1, self.atom2)
         self.assertEqual(bond.atoms(), set((self.atom1, self.atom2)))
         bond.atoms().remove(self.atom2)

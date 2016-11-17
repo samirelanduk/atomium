08ec080d8927ad65562ec04570e2714902fa2e0a
Sam Ireland <sam.ireland.uk@gmail.com>
2016-11-17 22:57:15 +0000

    Make Complex ResiduicStructure

--- molecupy/structures/complexes.py
+++ molecupy/structures/complexes.py
@@ -1,6 +1,23 @@
-class Complex:
+from . import ResiduicStructure
+
+class Complex(ResiduicStructure):
 
     def __init__(self, complex_id, complex_name, *chains):
         self._complex_id = complex_id
         self._complex_name = complex_name
         self._chains = set(chains)
+
+
+    def __getattr__(self, attribute):
+        if attribute == "_residues":
+            residues = set()
+            for chain in self._chains:
+                residues.update(chain.residues(include_missing=True))
+            return residues
+        elif attribute == "_atoms":
+            atoms = set()
+            for residue in self._residues:
+                atoms.update(residue.atoms(atom_type="all"))
+            return atoms
+        else:
+            return self.__getattribute__(attribute)
--- tests/structure_tests/test_complexes.py
+++ tests/structure_tests/test_complexes.py
@@ -1,17 +1,30 @@
 from unittest import TestCase
 from unittest.mock import Mock
-from molecupy.structures import Chain, Complex
+from molecupy.structures import Chain, Complex, ResiduicStructure, Residue, Atom
 
 class ComplexTest(TestCase):
 
     def setUp(self):
         self.chains = [Mock(Chain) for _ in range(3)]
+        self.residues = [Mock(ResiduicStructure) for _ in range(6)]
+        self.atoms = [Mock(Atom) for _ in range(18)]
+        for index, chain in enumerate(self.chains):
+            chain.residues.return_value = set(
+             self.residues[index * 2: (index * 2) + 2]
+            )
+        for index, residue in enumerate(self.residues):
+            residue.atoms.return_value = set(
+             self.atoms[index * 3: (index * 3) + 3]
+            )
 
 
 class ComplexCreationTests(ComplexTest):
 
     def test_can_create_complex(self):
         complex_ = Complex("1", "A Complex", *self.chains)
+        self.assertIsInstance(complex_, ResiduicStructure)
+        self.assertEqual(complex_.residues(), set(self.residues))
+        self.assertEqual(complex_.atoms(), set(self.atoms))
         self.assertEqual(complex_._complex_id, "1")
         self.assertEqual(complex_._complex_name, "A Complex")
         self.assertEqual(complex_._chains, set(self.chains))

75945a5121d15e7b0c11f2d590d13f588189b51e
Sam Ireland <sam.ireland.uk@gmail.com>
2017-01-06 18:47:04 +0000

    Move pdbdatafile-to-pdbfile conversion code to its own modle

---
+++ molecupy/converters/pdbdatafile2pdbfile.py
@@ -0,0 +1,138 @@
+import math
+from ..pdb.pdbfile import PdbFile, PdbRecord
+
+def pdb_file_from_pdb_data_file(data_file):
+    pdb_file = PdbFile()
+    create_compnd_records(pdb_file, data_file)
+    create_atom_records(pdb_file, data_file)
+    create_hetatm_records(pdb_file, data_file)
+    create_conect_records(pdb_file, data_file)
+    return pdb_file
+
+
+def create_compnd_records(pdb_file, data_file):
+    lines = []
+    for compound in data_file.compounds():
+        segments = []
+        if "MOL_ID" in compound:
+            segments.append("MOL_ID: %s;" % str(compound["MOL_ID"]))
+        if "MOLECULE" in compound:
+            segments.append("MOLECULE: %s;" % str(compound["MOLECULE"]))
+        if "CHAIN" in compound:
+            segments.append("CHAIN: %s;" % ", ".join(compound["CHAIN"]))
+        if "SYNONYM" in compound:
+            segments.append("SYNONYM: %s;" % ", ".join(compound["SYNONYM"]))
+        if "EC" in compound:
+            segments.append("EC: %s;" % str(compound["EC"]))
+        if "ENGINEERED" in compound:
+            segments.append("ENGINEERED: %s;" % "YES" if compound["ENGINEERED"] else "NO")
+        for segment in segments:
+            if len(segment) <= 69:
+                lines.append(segment)
+            else:
+                chunks = segment.split(" ")[::-1]
+                growing_line = ""
+                while chunks:
+                    chunk = chunks.pop()
+                    if len(growing_line + chunk) > 69:
+                        lines.append(growing_line)
+                        growing_line = chunk + " "
+                    else:
+                        growing_line += chunk + " "
+                    if not chunks:
+                        lines.append(growing_line)
+
+    for index, line in enumerate(lines):
+        pdb_file.add_record(PdbRecord("COMPND %s%s" % (
+         str(index + 1).rjust(3) + " " if index != 0 else "   ",
+         line
+        )))
+
+
+def create_atom_records(pdb_file, data_file):
+    for atom in data_file.atoms():
+        record_fragments = []
+        record_fragments.append("ATOM  ")
+        record_fragments.append("%5i" % atom["atom_id"] + " ")
+        record_fragments.append("%-4s" % atom["atom_name"][:4])
+        record_fragments.append(atom["alt_loc"][0] if atom["alt_loc"] else " ")
+        record_fragments.append(atom["residue_name"][0:3] + " " if atom["residue_name"] else "    ")
+        record_fragments.append(atom["chain_id"][0] if atom["chain_id"] else " ")
+        record_fragments.append(("%4i" % atom["residue_id"]) if atom["residue_id"] else "    ")
+        record_fragments.append(atom["insert_code"][0] + "   " if atom["insert_code"] else "    ")
+        record_fragments.append(_number_to_8_char_string(atom["x"]))
+        record_fragments.append(_number_to_8_char_string(atom["y"]))
+        record_fragments.append(_number_to_8_char_string(atom["z"]))
+        record_fragments.append(_number_to_6_char_string(atom["occupancy"]))
+        record_fragments.append(_number_to_6_char_string(atom["temperature_factor"]))
+        record_fragments.append(" " * 10)
+        record_fragments.append("%-2s" % atom["element"])
+        record_fragments.append(("%-2i" % atom["charge"]) if atom["charge"] else "  ")
+        pdb_file.add_record(PdbRecord("".join(record_fragments)))
+
+
+def create_hetatm_records(pdb_file, data_file):
+    for atom in data_file.heteroatoms():
+        record_fragments = []
+        record_fragments.append("HETATM")
+        record_fragments.append("%5i" % atom["atom_id"] + " ")
+        record_fragments.append("%-4s" % atom["atom_name"][:4])
+        record_fragments.append(atom["alt_loc"][0] if atom["alt_loc"] else " ")
+        record_fragments.append(atom["residue_name"][0:3] + " " if atom["residue_name"] else "    ")
+        record_fragments.append(atom["chain_id"][0] if atom["chain_id"] else " ")
+        record_fragments.append(("%4i" % atom["residue_id"]) if atom["residue_id"] else "    ")
+        record_fragments.append(atom["insert_code"][0] + "   " if atom["insert_code"] else "    ")
+        record_fragments.append(_number_to_8_char_string(atom["x"]))
+        record_fragments.append(_number_to_8_char_string(atom["y"]))
+        record_fragments.append(_number_to_8_char_string(atom["z"]))
+        record_fragments.append(_number_to_6_char_string(atom["occupancy"]))
+        record_fragments.append(_number_to_6_char_string(atom["temperature_factor"]))
+        record_fragments.append(" " * 10)
+        record_fragments.append("%-2s" % atom["element"])
+        record_fragments.append(("%-2i" % atom["charge"]) if atom["charge"] else "  ")
+        pdb_file.add_record(PdbRecord("".join(record_fragments)))
+
+
+def create_conect_records(pdb_file, data_file):
+    for connection in data_file.connections():
+        record_count = math.ceil(len(connection["bonded_atoms"]) / 4)
+        for n in range(record_count):
+            pdb_file.add_record(PdbRecord("CONECT%5i%5s%5s%5s%5s" % (
+             connection["atom_id"],
+             str(connection["bonded_atoms"][(n * 4)]) if (n * 4) < len(connection["bonded_atoms"]) else "",
+             str(connection["bonded_atoms"][(n * 4) + 1])
+              if (n * 4) + 1 < len(connection["bonded_atoms"]) else "",
+             str(connection["bonded_atoms"][(n * 4) + 2])
+              if (n * 4) + 2 < len(connection["bonded_atoms"]) else "",
+             str(connection["bonded_atoms"][(n * 4) + 3])
+              if (n * 4) + 3 < len(connection["bonded_atoms"]) else ""
+            )))
+
+
+def _number_to_8_char_string(number):
+    return _number_to_n_char_string(number, 8)
+
+
+def _number_to_6_char_string(number):
+    return _number_to_n_char_string(number, 6)
+
+
+def _number_to_n_char_string(number, n):
+    if number is None:
+        return " " * n
+    else:
+        number = round(number, n)
+        int_component = str(int(number))
+        if number < 0 and int_component[0] != "-":
+            int_component = "-" + int_component
+        float_component = number - int(number)
+        if len(int_component) >= 6 or float_component == 0 or "e" in str(float_component):
+            float_component = ".0"
+        else:
+            float_component = str(round(float_component, (n - 1) - len(int_component)))
+            if float_component[0] == "1":
+                float_component = ".0"
+                int_component = str(int(int_component) + 1)
+            else:
+                float_component = "." + float_component.split(".")[-1]
+        return (int_component + float_component).ljust(n)
--- molecupy/pdb/pdbdatafile.py
+++ molecupy/pdb/pdbdatafile.py
@@ -4,6 +4,7 @@ process the values that it extracts."""
 import datetime
 import math
 from .pdbfile import PdbRecord
+from ..converters.pdbdatafile2pdbfile import pdb_file_from_pdb_data_file
 
 class PdbDataFile:
     """This object is essentially a list of values extracted from a PDB file. It
@@ -800,123 +801,7 @@ class PdbDataFile:
             return self._master
 
 
-    def generate_compnd_records(self):
-        lines = []
-        for compound in self.compounds():
-            segments = []
-            if "MOL_ID" in compound:
-                segments.append("MOL_ID: %s;" % str(compound["MOL_ID"]))
-            if "MOLECULE" in compound:
-                segments.append("MOLECULE: %s;" % str(compound["MOLECULE"]))
-            if "CHAIN" in compound:
-                segments.append("CHAIN: %s;" % ", ".join(compound["CHAIN"]))
-            if "SYNONYM" in compound:
-                segments.append("SYNONYM: %s;" % ", ".join(compound["SYNONYM"]))
-            if "EC" in compound:
-                segments.append("EC: %s;" % str(compound["EC"]))
-            if "ENGINEERED" in compound:
-                segments.append("ENGINEERED: %s;" % "YES" if compound["ENGINEERED"] else "NO")
-            for segment in segments:
-                if len(segment) <= 69:
-                    lines.append(segment)
-                else:
-                    chunks = segment.split(" ")[::-1]
-                    growing_line = ""
-                    while chunks:
-                        chunk = chunks.pop()
-                        if len(growing_line + chunk) > 69:
-                            lines.append(growing_line)
-                            growing_line = chunk + " "
-                        else:
-                            growing_line += chunk + " "
-                        if not chunks:
-                            lines.append(growing_line)
-
-        return [PdbRecord("COMPND %s%s" % (
-         str(index + 1).rjust(3) + " " if index != 0 else "   ",
-         line
-        )) for index, line in enumerate(lines)]
-
-
-    def generate_atom_records(self):
-        records = []
-        for atom in self.atoms():
-            record_fragments = []
-            record_fragments.append("ATOM  ")
-            record_fragments.append("%5i" % atom["atom_id"] + " ")
-            record_fragments.append("%-4s" % atom["atom_name"][:4])
-            record_fragments.append(atom["alt_loc"][0] if atom["alt_loc"] else " ")
-            record_fragments.append(atom["residue_name"][0:3] + " " if atom["residue_name"] else "    ")
-            record_fragments.append(atom["chain_id"][0] if atom["chain_id"] else " ")
-            record_fragments.append(("%4i" % atom["residue_id"]) if atom["residue_id"] else "    ")
-            record_fragments.append(atom["insert_code"][0] + "   " if atom["insert_code"] else "    ")
-            record_fragments.append(_number_to_8_char_string(atom["x"]))
-            record_fragments.append(_number_to_8_char_string(atom["y"]))
-            record_fragments.append(_number_to_8_char_string(atom["z"]))
-            record_fragments.append(_number_to_6_char_string(atom["occupancy"]))
-            record_fragments.append(_number_to_6_char_string(atom["temperature_factor"]))
-            record_fragments.append(" " * 10)
-            record_fragments.append("%-2s" % atom["element"])
-            record_fragments.append(("%-2i" % atom["charge"]) if atom["charge"] else "  ")
-            records.append(PdbRecord("".join(record_fragments)))
-        return records
-
-
-    def generate_hetatm_records(self):
-        records = []
-        for atom in self.heteroatoms():
-            record_fragments = []
-            record_fragments.append("HETATM")
-            record_fragments.append("%5i" % atom["atom_id"] + " ")
-            record_fragments.append("%-4s" % atom["atom_name"][:4])
-            record_fragments.append(atom["alt_loc"][0] if atom["alt_loc"] else " ")
-            record_fragments.append(atom["residue_name"][0:3] + " " if atom["residue_name"] else "    ")
-            record_fragments.append(atom["chain_id"][0] if atom["chain_id"] else " ")
-            record_fragments.append(("%4i" % atom["residue_id"]) if atom["residue_id"] else "    ")
-            record_fragments.append(atom["insert_code"][0] + "   " if atom["insert_code"] else "    ")
-            record_fragments.append(_number_to_8_char_string(atom["x"]))
-            record_fragments.append(_number_to_8_char_string(atom["y"]))
-            record_fragments.append(_number_to_8_char_string(atom["z"]))
-            record_fragments.append(_number_to_6_char_string(atom["occupancy"]))
-            record_fragments.append(_number_to_6_char_string(atom["temperature_factor"]))
-            record_fragments.append(" " * 10)
-            record_fragments.append("%-2s" % atom["element"])
-            record_fragments.append(("%-2i" % atom["charge"]) if atom["charge"] else "  ")
-            records.append(PdbRecord("".join(record_fragments)))
-        return records
-
-
-    def generate_conect_records(self):
-        records = []
-        for connection in self.connections():
-            record_count = math.ceil(len(connection["bonded_atoms"]) / 4)
-            for n in range(record_count):
-                records.append(PdbRecord("CONECT%5i%5s%5s%5s%5s" % (
-                 connection["atom_id"],
-                 str(connection["bonded_atoms"][(n * 4)]) if (n * 4) < len(connection["bonded_atoms"]) else "",
-                 str(connection["bonded_atoms"][(n * 4) + 1])
-                  if (n * 4) + 1 < len(connection["bonded_atoms"]) else "",
-                 str(connection["bonded_atoms"][(n * 4) + 2])
-                  if (n * 4) + 2 < len(connection["bonded_atoms"]) else "",
-                 str(connection["bonded_atoms"][(n * 4) + 3])
-                  if (n * 4) + 3 < len(connection["bonded_atoms"]) else ""
-                )))
-        return records
-
-
-    def generate_pdb_file(self):
-        from .pdbfile import PdbFile
-        pdb_file = PdbFile()
-        for record in self.generate_compnd_records():
-            pdb_file.add_record(record)
-        for record in self.generate_atom_records():
-            pdb_file.add_record(record)
-        for record in self.generate_hetatm_records():
-            pdb_file.add_record(record)
-        for record in self.generate_conect_records():
-            pdb_file.add_record(record)
-        return pdb_file
-
+    generate_pdb_file = pdb_file_from_pdb_data_file
 
 
 def _process_header_records(data_file):
@@ -1813,32 +1698,3 @@ def records_to_token_value_dicts(records):
         entity[pair[0]] = pair[1]
     if entity: entities.append(entity)
     return entities
-
-
-def _number_to_8_char_string(number):
-    return _number_to_n_char_string(number, 8)
-
-
-def _number_to_6_char_string(number):
-    return _number_to_n_char_string(number, 6)
-
-
-def _number_to_n_char_string(number, n):
-    if number is None:
-        return " " * n
-    else:
-        number = round(number, n)
-        int_component = str(int(number))
-        if number < 0 and int_component[0] != "-":
-            int_component = "-" + int_component
-        float_component = number - int(number)
-        if len(int_component) >= 6 or float_component == 0 or "e" in str(float_component):
-            float_component = ".0"
-        else:
-            float_component = str(round(float_component, (n - 1) - len(int_component)))
-            if float_component[0] == "1":
-                float_component = ".0"
-                int_component = str(int(int_component) + 1)
-            else:
-                float_component = "." + float_component.split(".")[-1]
-        return (int_component + float_component).ljust(n)

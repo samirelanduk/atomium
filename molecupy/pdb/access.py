57b7e764623fe4459421796387ba88e91a0466bc
Sam Ireland <sam.ireland.uk@gmail.com>
2017-01-14 21:27:04 +0000

    Add quick-access functions for getting PdbDataFiles and PdbFiles from files and web services

--- molecupy/pdb/access.py
+++ molecupy/pdb/access.py
@@ -14,33 +14,47 @@ def pdb_from_string(text):
     :param str string: The raw text of a PDB file.
     :rtype: :py:class:`.Pdb`"""
 
-    return Pdb(PdbDataFile(PdbFile(text)))
+    return Pdb(PdbFile(text).to_pdb_data_file())
 
 
-def get_pdb_from_file(path):
-    """Gets a :py:class:`.Pdb` object from a PDB file stored on disk.
+def pdb_data_file_from_string(text):
+    return PdbFile(text).to_pdb_data_file()
 
-    :param str path: The path to the PDB file.
-    :rtype: :py:class:`.Pdb`"""
 
+def pdb_file_from_string(text):
+    return PdbFile(text)
+
+
+def get_pdb_from_file(path, processing="pdb"):
+    if processing not in ("pdb", "data_file", "pdb_file"):
+        raise ValueError(
+         "Only valid processing arguments are 'pdb', 'data_file' and 'pdb_file'"
+        )
     with open(path) as f:
-        return pdb_from_string(f.read())
+        if processing == "pdb_file":
+            return pdb_file_from_string(f.read())
+        elif processing == "data_file":
+            return pdb_data_file_from_string(f.read())
+        else:
+            return pdb_from_string(f.read())
 
 
-def get_pdb_remotely(code):
-    """Gets a :py:class:`.Pdb` object from a PDB code.
-    The file is requested from the RCSB servers via a HTTP request.
-
-    :param str code: The PDB code required - e.g. '1NVQ'.
-    :rtype: :py:class:`.Pdb`
-    :raises: :class:`.InvalidPdbCodeError` if the PDB doesn't exist"""
-
+def get_pdb_remotely(code, processing="pdb"):
+    if processing not in ("pdb", "data_file", "pdb_file"):
+        raise ValueError(
+         "Only valid processing arguments are 'pdb', 'data_file' and 'pdb_file'"
+        )
     response = requests.get(
      "http://www.ebi.ac.uk/pdbe/entry-files/pdb%s.ent" % code.lower()
     )
     if response.status_code == 200 and response.text[:6] == "HEADER":
         contents = response.text
-        return pdb_from_string(contents)
+        if processing == "pdb_file":
+            return pdb_file_from_string(contents)
+        elif processing == "data_file":
+            return pdb_data_file_from_string(contents)
+        else:
+            return pdb_from_string(contents)
     else:
         raise InvalidPdbCodeError(
          "%s does not seem to be a valid PDB code." % code
--- tests/pdb_tests/test_access.py
+++ tests/pdb_tests/test_access.py
@@ -2,7 +2,7 @@ import os
 from unittest import TestCase
 from unittest.mock import patch
 import unittest
-from molecupy import pdb_from_string, get_pdb_from_file, get_pdb_remotely
+from molecupy.pdb.access import *
 from molecupy.pdb.pdb import Pdb
 from molecupy.exceptions import InvalidPdbCodeError
 
@@ -15,6 +15,23 @@ class PdbFromStringTests(TestCase):
         self.assertEqual(pdb.keywords(), ["TIM BARREL", "LYASE"])
 
 
+    def test_can_make_pdb_data_file_from_string(self):
+        text = "KEYWDS    TIM BARREL, LYASE"
+        pdb = pdb_data_file_from_string(text)
+        self.assertIsInstance(pdb, PdbDataFile)
+        self.assertEqual(pdb.keywords(), ["TIM BARREL", "LYASE"])
+
+
+    def test_can_make_pdb_file_from_string(self):
+        text = "KEYWDS    TIM BARREL, LYASE"
+        pdb = pdb_file_from_string(text)
+        self.assertIsInstance(pdb, PdbFile)
+        self.assertEqual(
+         pdb.get_record_by_name("KEYWDS").text(),
+         "KEYWDS    TIM BARREL, LYASE" + (" " * 53)
+        )
+
+
 
 class PdbFromFileTests(TestCase):
 
@@ -24,9 +41,37 @@ class PdbFromFileTests(TestCase):
         self.assertEqual(pdb.keywords(), ["TIM BARREL", "LYASE"])
 
 
+    def test_can_make_pdb_data_file_from_file(self):
+        pdb = get_pdb_from_file(
+         os.path.sep.join(["tests", "pdb_tests", "1LOL.pdb"]),
+         processing="data_file"
+        )
+        self.assertIsInstance(pdb, PdbDataFile)
+        self.assertEqual(pdb.keywords(), ["TIM BARREL", "LYASE"])
+
+
+    def test_can_make_pdb_file_from_file(self):
+        pdb = get_pdb_from_file(
+         os.path.sep.join(["tests", "pdb_tests", "1LOL.pdb"]),
+         processing="pdb_file"
+        )
+        self.assertIsInstance(pdb, PdbFile)
+        self.assertEqual(
+         pdb.get_record_by_name("KEYWDS").text(),
+         "KEYWDS    TIM BARREL, LYASE" + (" " * 53)
+        )
+
+
+    def test_processing_arg_must_be_valid(self):
+        with self.assertRaises(ValueError):
+            pdb = get_pdb_from_file(
+             os.path.sep.join(["tests", "pdb_tests", "1LOL.pdb"]),
+             processing="....."
+            )
+
+
 
 class PdbRemoteTests(TestCase):
-
 
     @patch("requests.get")
     def test_can_get_pdb_remotely(self, mock_get):
@@ -38,6 +83,38 @@ class PdbRemoteTests(TestCase):
         pdb = get_pdb_remotely("1LOL")
         self.assertIsInstance(pdb, Pdb)
         self.assertEqual(pdb.keywords(), ["TIM BARREL", "LYASE"])
+
+
+    @patch("requests.get")
+    def test_can_get_pdb_data_file_remotely(self, mock_get):
+        response = unittest.mock.Mock()
+        with open(os.path.sep.join(["tests", "pdb_tests", "1LOL.pdb"])) as f:
+            response.text = f.read()
+        response.status_code = 200
+        mock_get.return_value = response
+        pdb = get_pdb_remotely("1LOL", processing="data_file")
+        self.assertIsInstance(pdb, PdbDataFile)
+        self.assertEqual(pdb.keywords(), ["TIM BARREL", "LYASE"])
+
+
+    @patch("requests.get")
+    def test_can_get_pdb_file_remotely(self, mock_get):
+        response = unittest.mock.Mock()
+        with open(os.path.sep.join(["tests", "pdb_tests", "1LOL.pdb"])) as f:
+            response.text = f.read()
+        response.status_code = 200
+        mock_get.return_value = response
+        pdb = get_pdb_remotely("1LOL", processing="pdb_file")
+        self.assertIsInstance(pdb, PdbFile)
+        self.assertEqual(
+         pdb.get_record_by_name("KEYWDS").text(),
+         "KEYWDS    TIM BARREL, LYASE" + (" " * 53)
+        )
+
+
+    def test_processing_arg_must_be_valid(self):
+        with self.assertRaises(ValueError):
+            pdb = get_pdb_remotely("1LOL", processing=".....")
 
 
     @patch("requests.get")

6a632cd8d07e347dc869aad9b96d4988723db4e5
Sam Ireland <sam.ireland.uk@gmail.com>
2017-01-21 16:45:51 +0000

    Change release from 2.0 to 1.1

--- molecupy/__init__.py
+++ molecupy/__init__.py
@@ -1,5 +1,5 @@
 from .exceptions import *
 from .pdb.access import get_pdb_remotely, get_pdb_from_file, pdb_from_string
 
-__version__ = "2.0.0"
+__version__ = "1.1.0"
 __author__ = "Sam Ireland"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
  name="molecupy",
- version="2.0.0",
+ version="1.1.0",
  description="A Python molecular modeller with PDB parsing.",
  url="https://molecupy.readthedocs.io",
  author="Sam Ireland",

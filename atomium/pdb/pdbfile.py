4285a6c95900ce318aeecbed363ef186261fdbab
Sam Ireland <sam.ireland.uk@gmail.com>
2017-04-09 18:30:50 +0100

    Make PdbRecord validate input

--- atomium/pdb/pdbfile.py
+++ atomium/pdb/pdbfile.py
@@ -1,4 +1,8 @@
 class PdbRecord:
 
     def __init__(self, line):
+        if not isinstance(line, str):
+            raise TypeError("PdbRecord needs str, not '%s'" % str(line))
+        if len(line) > 80:
+            raise ValueError("'%s' is longer than 80 characters" % str(line))
         self._text = line
--- tests/pdb_tests/test_pdb_records.py
+++ tests/pdb_tests/test_pdb_records.py
@@ -6,3 +6,14 @@ class PdbRecordCreationTests(TestCase):
     def test_can_create_pdb_record(self):
         record = PdbRecord("RECORD XXX YYY ZZZ 01")
         self.assertEqual(record._text, "RECORD XXX YYY ZZZ 01")
+
+
+    def test_pdb_record_needs_string(self):
+        with self.assertRaises(TypeError):
+            PdbRecord(100)
+
+
+    def test_pdb_record_must_be_less_than_80_chars(self):
+        PdbRecord("." * 80)
+        with self.assertRaises(ValueError):
+            PdbRecord("." * 81)

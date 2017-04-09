3d197d750c97305b7bf4a06f934fd950264ef353
Sam Ireland <sam.ireland.uk@gmail.com>
2017-04-09 18:44:56 +0100

    Add docstrings to PdbRecord

--- atomium/pdb/pdbfile.py
+++ atomium/pdb/pdbfile.py
@@ -1,4 +1,10 @@
 class PdbRecord:
+    """A PdbRecord represents a single line in a PDB file. As such, it follows
+    the same constraints that the PDB file formats impose on the lines in the
+    actual files - namely that it cannot be more than 80 characters.
+
+    :param str text: The string contents of the line in the PDB file.
+    :raises ValueError: if a string longer than 80 characters is supplied."""
 
     def __init__(self, text):
         if not isinstance(text, str):
@@ -9,6 +15,12 @@ class PdbRecord:
 
 
     def text(self, text=None):
+        """The full string of the record. If a string is passed as an argument,
+        the text will be updated.
+
+        :param str text: If given, the record's text will be updated to this.
+        :raises ValueError: if a string longer than 80 characters is supplied."""
+
         if text is None:
             return self._text
         else:

aa5cd7513768f9e7dccd1252406c566eef52a6a6
Sam Ireland <sam.ireland.uk@gmail.com>
2017-05-05 12:35:57 +0100

    Add Atom validation

--- atomium/structures/atoms.py
+++ atomium/structures/atoms.py
@@ -1,15 +1,31 @@
+from matrices.checks import is_numeric
+
 """Contains the Atom class and its associated classes."""
 
 class Atom:
     """Represents atoms in three dimensional space. Every atom has an element
     and a set of Cartesian coordinates.
 
-    :param str element: The atom's element symbol.
+    :param str element: The atom's element symbol. It doesn't need to be on the\
+    Periodic Table, but it does need to be 1 or 2 characters.
     :param number x: The atom's x coordinate.
     :param number y: The atom's y coordinate.
-    :param number z: The atom's z coordinate."""
+    :param number z: The atom's z coordinate.
+    :raises TypeError: if the element is not str.
+    :raises ValueError: if the element is not 1 or 2 characters.
+    :raises TypeError: if the coordinates are not numeric."""
 
     def __init__(self, element, x, y, z):
+        if not isinstance(element, str):
+            raise TypeError("Element '{}' is not str".format(element))
+        if not 0 < len(element) < 3:
+            raise ValueError("Element {} is not 1 or 2 chars".format(element))
+        if not is_numeric(x):
+            raise TypeError("x coordinate '{}' is not numeric".format(x))
+        if not is_numeric(y):
+            raise TypeError("y coordinate '{}' is not numeric".format(y))
+        if not is_numeric(z):
+            raise TypeError("z coordinate '{}' is not numeric".format(z))
         self._element = element
         self._x = x
         self._y = y
--- tests/structure_tests/test_atoms.py
+++ tests/structure_tests/test_atoms.py
@@ -9,3 +9,35 @@ class AtomCreationTests(TestCase):
         self.assertEqual(atom._x, 2)
         self.assertEqual(atom._y, 3)
         self.assertEqual(atom._z, 5)
+
+
+    def test_atom_element_must_be_str(self):
+        with self.assertRaises(TypeError):
+            Atom(1, 2, 3, 5)
+
+
+    def test_atom_element_must_be_1_or_2_chars(self):
+        with self.assertRaises(ValueError):
+            Atom("", 2, 3, 5)
+        with self.assertRaises(ValueError):
+            Atom("XXX", 2, 3, 5)
+        Atom("XX", 2, 3, 5)
+        Atom("X", 2, 3, 5)
+
+
+    def test_atom_x_coord_must_be_number(self):
+        with self.assertRaises(TypeError):
+            Atom("C", "2", 3, 5)
+        Atom("C", 2.5, 3, 5)
+
+
+    def test_atom_y_coord_must_be_number(self):
+        with self.assertRaises(TypeError):
+            Atom("C", 2, "3", 5)
+        Atom("C", 2, 3.5, 5)
+
+
+    def test_atom_z_coord_must_be_number(self):
+        with self.assertRaises(TypeError):
+            Atom("C", 2, 3, "5")
+        Atom("C", 2, 3, 5.5)

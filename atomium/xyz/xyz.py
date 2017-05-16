b3025fd749c1cb9d6c322b124899109be1120c96
Sam Ireland <sam.ireland.uk@gmail.com>
2017-05-16 10:08:31 +0100

    Add xyz comment property

--- atomium/xyz/xyz.py
+++ atomium/xyz/xyz.py
@@ -15,3 +15,18 @@ class Xyz:
 
     def __repr__(self):
         return "<Xyz ({})>".format(self._comment)
+
+
+    def comment(self, comment=None):
+        """Returns the comment associated with the .xyz file. If a string is
+        given, the comment will be updated.
+
+        :param str comment: If given, the commnent will be updated to this.
+        :raises TypeError: if the comment given is not a string."""
+        
+        if comment is None:
+            return self._comment
+        else:
+            if not isinstance(comment, str):
+                raise TypeError("comment must be str, not '{}'".format(comment))
+            self._comment = comment
--- tests/xyz_tests/test_xyz.py
+++ tests/xyz_tests/test_xyz.py
@@ -26,3 +26,23 @@ class XyzReprTests(TestCase):
     def test_xyz_repr(self):
         xyz = Xyz("Glucose molecule")
         self.assertEqual(str(xyz), "<Xyz (Glucose molecule)>")
+
+
+
+class XyzCommentTests(TestCase):
+
+    def test_comment_property(self):
+        xyz = Xyz("Glucose molecule")
+        self.assertIs(xyz._comment, xyz.comment())
+
+
+    def test_can_change_comment(self):
+        xyz = Xyz("Glucose molecule")
+        xyz.comment("Fructose molecule")
+        self.assertEqual(xyz._comment, "Fructose molecule")
+
+
+    def test_xyz_comment_must_be_str(self):
+        xyz = Xyz("Glucose molecule")
+        with self.assertRaises(TypeError):
+            xyz.comment(100)

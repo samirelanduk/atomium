70444a03b4860aa11548f9c11d689d2964350e74
Sam Ireland <sam.ireland.uk@gmail.com>
2017-05-18 11:41:46 +0100

    Finish string to line conversion

--- atomium/converters/strings.py
+++ atomium/converters/strings.py
@@ -2,4 +2,10 @@
 forms, and back into strings."""
 
 def string2lines(s):
-    return [s]
+    """Takes a string and breaks it into lines on line breaks. It will first
+    convert any windows line breaks to unix line breaks.
+
+    :param str s: The string to break up.
+    :rtype: ``str``."""
+    
+    return s.replace("\r\n", "\n").split("\n")
--- tests/converter_tests/test_string_processing.py
+++ tests/converter_tests/test_string_processing.py
@@ -5,3 +5,11 @@ class String2LinesTests(TestCase):
 
     def test_line_becomes_line(self):
         self.assertEqual(string2lines("A line."), ["A line."])
+
+
+    def test_can_break_string_on_newline(self):
+        self.assertEqual(string2lines("A line.\nB line."), ["A line.", "B line."])
+
+
+    def test_can_handle_windows_linebreaks(self):
+        self.assertEqual(string2lines("A line.\r\nB line."), ["A line.", "B line."])

eeb24567e77ed4cd4d561e549e8f293979fe6585
Sam Ireland <sam.ireland.uk@gmail.com>
2017-05-21 11:18:50 +0100

    Add string to xyz parsing

--- atomium/converters/string2xyz.py
+++ atomium/converters/string2xyz.py
@@ -1,12 +1,30 @@
 from ..xyz.xyz import Xyz
 from ..structures.models import Model
+from ..structures.atoms import Atom
+from .strings import string2lines
 
 def string_to_xyz(s):
     """Converts a string taken from a .xyz file and turns it into a
     :py:class:`.Xyz` object.
 
     :param str s: The string to convert."""
-    
+
+    lines = string2lines(s)
     xyz = Xyz()
+    if len(lines) > 2:
+        try:
+            int(lines[0])
+            lines = lines[1:]
+        except:
+            pass
+        try:
+            element, x, y, z = lines[0].split()
+            float(x), float(y), float(z)
+        except:
+            xyz._comment = lines[0]
+            lines = lines[1:]
     xyz._model = Model()
+    for line in lines:
+        element, x, y, z = line.split()
+        xyz._model._atoms.add(Atom(element, float(x), float(y), float(z)))
     return xyz
--- tests/converter_tests/test_string_to_xyz.py
+++ tests/converter_tests/test_string_to_xyz.py
@@ -5,8 +5,62 @@ from atomium.xyz.xyz import Xyz
 
 class StringToXyzTests(TestCase):
 
-    def test_empty_string_is_blank_xyz(self):
+    @patch("atomium.converters.string2xyz.string2lines")
+    def test_empty_string_is_blank_xyz(self, mock_lines):
+        mock_lines.return_value = []
         xyz = string_to_xyz("")
+        mock_lines.assert_called_with("")
         self.assertIsInstance(xyz, Xyz)
         self.assertEqual(xyz._comment, "")
         self.assertEqual(xyz._model._atoms, set())
+
+
+    @patch("atomium.converters.string2xyz.string2lines")
+    def test_basic_xyz_parsing(self, mock_lines):
+        string = ("12\n"
+        "glucose from 2gbp\n"
+        "C  35.884  30.895  49.120\n"
+        "C  36.177  29.853  50.124")
+        mock_lines.return_value = string.split("\n")
+        xyz = string_to_xyz(string)
+        mock_lines.assert_called_with(string)
+        self.assertEqual(xyz._comment, "glucose from 2gbp")
+        atoms = xyz._model._atoms
+        self.assertEqual([atom._element for atom in atoms], ["C", "C"])
+        self.assertEqual(
+         set(((35.884, 30.895, 49.120), (36.177, 29.853, 50.124))),
+         set([(atom._x, atom._y, atom._z) for atom in atoms])
+        )
+
+
+    @patch("atomium.converters.string2xyz.string2lines")
+    def test_can_skip_numatom_parsing(self, mock_lines):
+        string = ("glucose from 2gbp\n"
+        "C  35.884  30.895  49.120\n"
+        "C  36.177  29.853  50.124")
+        mock_lines.return_value = string.split("\n")
+        xyz = string_to_xyz(string)
+        mock_lines.assert_called_with(string)
+        self.assertEqual(xyz._comment, "glucose from 2gbp")
+        atoms = xyz._model._atoms
+        self.assertEqual([atom._element for atom in atoms], ["C", "C"])
+        self.assertEqual(
+         set(((35.884, 30.895, 49.120), (36.177, 29.853, 50.124))),
+         set([(atom._x, atom._y, atom._z) for atom in atoms])
+        )
+
+
+    @patch("atomium.converters.string2xyz.string2lines")
+    def test_can_skip_comment(self, mock_lines):
+        string = ("C  35.884  30.895  49.120\n"
+        "C  36.177  29.853  50.124")
+        mock_lines.return_value = string.split("\n")
+        xyz = string_to_xyz(string)
+        mock_lines.assert_called_with(string)
+        self.assertEqual(xyz._comment, "")
+        atoms = xyz._model._atoms
+        self.assertEqual([atom._element for atom in atoms], ["C", "C"])
+        self.assertEqual(
+         set(((35.884, 30.895, 49.120), (36.177, 29.853, 50.124))),
+         set([(atom._x, atom._y, atom._z) for atom in atoms])
+        )

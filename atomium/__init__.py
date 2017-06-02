595b7413a90848be028ad2cadd390231dcced362
Sam Ireland <sam.ireland.uk@gmail.com>
2017-06-02 11:31:35 +0100

    Start 0.2

--- atomium/__init__.py
+++ atomium/__init__.py
@@ -1,4 +1,4 @@
 from .xyz import xyz_from_file
 
 __author__ = "Sam Ireland"
-__version__ = "0.1.1"
+__version__ = "0.2.0"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
  name="atomium",
- version="0.1.1",
+ version="0.2.0",
  description="A molecular modeller.",
  url="https://atomium.samireland.com",
  author="Sam Ireland",

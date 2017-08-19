52c125cd2725d3a7001d3c760f94d1ec3943130f
Sam Ireland <sam.ireland.uk@gmail.com>
2017-08-19 11:42:02 +0100

    Add PdbDataFile to Pdb conversion

---
+++ atomium/converters/pdbdatafile2pdb.py
@@ -0,0 +1,14 @@
+"""Contains the function for creating Pdbs from PdbDataFiles."""
+
+from .pdbdatafile2model import pdb_data_file_to_model
+from ..parse.pdb import Pdb
+
+def pdb_data_file_to_pdb(data_file):
+    """Converts a :py:class:`.PdbDataFile` to a :py:class:`.Pdb`
+
+    :param PdbDataFile data_file: The PdbDataFile.
+    :rtype: ``Pdb``"""
+
+    pdb = Pdb()
+    pdb._model = pdb_data_file_to_model(data_file)
+    return pdb
---
+++ tests/converter_tests/test_pdb_data_file_to_pdb.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from unittest.mock import patch, Mock
+from atomium.converters.pdbdatafile2pdb import *
+from atomium.parse.pdbdatafile import PdbDataFile
+from atomium.parse.pdb import Pdb
+
+class PdbDataFileToPdbTests(TestCase):
+
+    @patch("atomium.converters.pdbdatafile2pdb.pdb_data_file_to_model")
+    def test_can_get_pdb_from_data_file(self, mock_model):
+        model = Mock()
+        mock_model.return_value = model
+        data_file = Mock(PdbDataFile)
+        pdb = pdb_data_file_to_pdb(data_file)
+        self.assertIsInstance(pdb, Pdb)
+        self.assertIs(pdb._model, model)
+        mock_model.assert_called_with(data_file)

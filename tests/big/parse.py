b9e94dc9d4f0bd982960b01ceed80bcf695ad636
Sam Ireland <sam.ireland.uk@gmail.com>
2017-09-19 14:55:29 +0100

    Start big tests

---
+++ tests/big/parse.py
@@ -0,0 +1,17 @@
+
+import sys
+sys.path.insert(0, ".")
+import atomium
+from big import get_all_codes
+
+print("Getting PDB codes...")
+codes = get_all_codes()
+print("There are {} codes.".format(len(codes)))
+
+print("Parsing...")
+for code in codes:
+    print("\tParsing {}...".format(code))
+    pdb = atomium.fetch(code)
+    print("\tSuccess ({} model{}).\n".format(
+     len(pdb.models()), "" if len(pdb.models()) == 1 else "s"
+    ))

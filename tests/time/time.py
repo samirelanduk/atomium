507084df7c4e74ec7b68f37972e01a550cc4495c
Sam Ireland <sam.ireland.uk@gmail.com>
2017-09-30 23:59:51 +0100

    Make time tests ignore requests

--- tests/time/time.py
+++ tests/time/time.py
@@ -4,6 +4,7 @@ import datetime
 import sys
 sys.path.insert(0, ".")
 import atomium
+import requests
 
 here = dir_path = os.path.dirname(os.path.realpath(__file__))
 version = atomium.__version__.replace(".", "-")
@@ -11,6 +12,11 @@ today = datetime.datetime.now().date().strftime("%Y-%m-%d")
 
 pdbs = ["1LOL", "5XME"]
 for pdb in pdbs:
+    filestring = requests.get(
+     "http://www.ebi.ac.uk/pdbe/entry-files/pdb{}.ent".format(pdb.lower())
+    ).text
+    with open("{}/{}.pdb".format(here, pdb), "w") as f:
+        f.write(filestring)
     with open("{}/timescript.py".format(here)) as f:
         script = f.read().format(pdb)
     with open("{}/{}.py".format(here, pdb), "w") as f:
@@ -23,4 +29,5 @@ for pdb in pdbs:
         )
     finally:
         os.remove("{}/{}.py".format(here, pdb))
+        os.remove("{}/{}.pdb".format(here, pdb))
         os.remove("{}/temp.pdb".format(here))
--- tests/time/timescript.py
+++ tests/time/timescript.py
@@ -2,5 +2,5 @@ import sys
 sys.path.insert(0, ".")
 import atomium
 
-pdb = atomium.fetch("{}", pdbe=True)
+pdb = atomium.pdb_from_file("tests/time/{}.pdb")
 pdb.save("tests/time/temp.pdb")

419f3b69c0b0bb9ae06b5cc972c6b71230e070ae
Sam Ireland <sam.ireland.uk@gmail.com>
2018-05-23 12:11:38 +0100

    Start 0.10

--- atomium/__init__.py
+++ atomium/__init__.py
@@ -3,4 +3,4 @@ from .files import pdb_data_from_file, fetch_data
 from .files import pdb_from_file, fetch
 
 __author__ = "Sam Ireland"
-__version__ = "0.9.1"
+__version__ = "0.10.0"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
  name="atomium",
- version="0.9.1",
+ version="0.10.0",
  description="A molecular modeller and file parser.",
  url="https://atomium.samireland.com",
  author="Sam Ireland",

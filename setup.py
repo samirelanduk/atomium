d2172f160cb22a67ad6d9d208e5fba732a0e7c0b
Sam Ireland <sam.ireland.uk@gmail.com>
2018-07-13 17:18:28 +0100

    Start 0.10.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
  name="atomium",
- version="0.10.1",
+ version="0.10.2",
  description="A molecular modeller and file parser.",
  url="https://atomium.samireland.com",
  author="Sam Ireland",

72d8f5dfcc5ca505d224135f511e0f5db3c9ab19
Sam Ireland <sam.ireland.uk@gmail.com>
2018-11-14 15:22:29 +0000

    Drop support for Python 3.5 (sorry)

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setup(
   "License :: OSI Approved :: MIT License",
   "Topic :: Scientific/Engineering :: Chemistry",
   "Programming Language :: Python :: 3",
-  "Programming Language :: Python :: 3.5",
   "Programming Language :: Python :: 3.6",
   "Programming Language :: Python :: 3.7",
  ],

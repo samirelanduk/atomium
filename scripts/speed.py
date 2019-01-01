721cb9a71b291414e1e80600b51edd79a1160253
Sam Ireland <sam.ireland.uk@gmail.com>
2019-01-01 17:02:28 +0000

    Add speed comparison script

---
+++ scripts/speed.py
@@ -0,0 +1,106 @@
+import requests
+import random
+import sys
+import os
+import json
+from datetime import datetime
+sys.path.append(os.path.join("..", "atomium"))
+from atomium.utilities import parse_string
+import io
+from Bio.PDB import *
+import matplotlib.pyplot as plt
+
+def get_string(code):
+    if code.endswith(".mmtf"):
+        url = "https://mmtf.rcsb.org/v1.0/full/{}".format(code[:-5].lower())
+    else:
+        url = "https://files.rcsb.org/view/" + code.lower()
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        text = response.content if code.endswith(".mmtf") else response.text
+    return text
+
+if len(sys.argv) > 1 and sys.argv[1] == "-rebuild":
+    query = "<orgPdbQuery>"\
+    "<queryType>org.pdb.query.simple.ChemCompFormulaQuery</queryType>"\
+    "<formula>ZN</formula></orgPdbQuery>"
+    url = "https://www.rcsb.org//pdb/rest/search/"
+    response = requests.post(url, data=query.encode(), headers={
+     "Content-Type": "application/x-www-form-urlencoded"
+    })
+    codes = response.text.split()
+
+    data = []
+    while len (data) != 1000:
+        code = random.choice(codes)
+        d = {"code": code}
+        print(len(data) + 1, code)
+        for ext in [".cif", ".mmtf", ".pdb"]:
+            try:
+                string = get_string(code + ext)
+                start = datetime.now()
+                pdb = parse_string(string, path=ext)
+                end = datetime.now()
+                delta = end - start
+                d[ext] = delta.total_seconds()
+                if "atoms" not in d:
+                    d["atoms"] = len(pdb.model.atoms())
+                if "models" not in d:
+                    d["models"] = len(pdb.models)
+            except:
+                d[ext] = None
+
+        if d[".pdb"]:
+            try:
+                parser = PDBParser(QUIET=True, get_header=True)
+                start = datetime.now()
+                parser.get_structure("", io.StringIO(string))
+                end = datetime.now()
+                delta = end - start
+                d["biopython"] = delta.total_seconds()
+            except:
+                d["biopython"] = None
+
+        else:
+            d["biopython"] = None
+
+        data.append(d)
+        codes.remove(code)
+
+        with open("scripts/speed.json", "w") as f:
+            json.dump(data, f)
+
+with open("scripts/speed.json") as f:
+    data = json.load(f)[:500]
+
+print("There are {} data points".format(len(data)))
+
+pdbs_x, pdbs_y = zip(*[[d["atoms"], d[".pdb"]] for d in data if d[".pdb"] and d["models"] == 1 and d[".pdb"] < 20])
+cifs_x, cifs_y = zip(*[[d["atoms"], d[".cif"]] for d in data if d[".cif"] and d["models"] == 1 and d[".cif"] < 20])
+mmtfs_x, mmtfs_y = zip(*[[d["atoms"], d[".mmtf"]] for d in data if d[".mmtf"] and d["models"] == 1 and d[".mmtf"] < 20])
+bios_x, bios_y = zip(*[[d["atoms"], d["biopython"]] for d in data if d["biopython"] and d["models"] == 1 and d["biopython"] < 20])
+
+plt.xscale("log")
+plt.yscale("log")
+plt.scatter(pdbs_x, pdbs_y, s=6, c="#58B19F", label=".pdb", alpha=0.8, linewidths=0)
+plt.scatter(cifs_x, cifs_y, s=6, c="#FD7272", label=".cif", alpha=0.8, linewidths=0)
+plt.scatter(mmtfs_x, mmtfs_y, s=6, c="#182C61", label=".mmtf", alpha=0.8, linewidths=0)
+plt.xlabel("Atom Count")
+plt.ylabel("Parse time (s)")
+plt.legend()
+plt.savefig("scripts/compare.png", dpi=1000)
+plt.clf()
+
+
+plt.xscale("log")
+plt.yscale("log")
+plt.scatter(pdbs_x, pdbs_y, s=6, c="#58B19F", label=".pdb (atomium)", alpha=0.8, linewidths=0)
+plt.scatter(bios_x, bios_y, s=6, c="#D6A2E8", label=".pdb (biopython)", alpha=0.8, linewidths=0)
+plt.xlabel("Atom Count")
+plt.ylabel("Parse time (s)")
+plt.legend()
+plt.savefig("scripts/compare2.png", dpi=1000)
+plt.clf()
+
+
+#plt.scatter(bios_x, bios_y, s=4, c="#F97F51")

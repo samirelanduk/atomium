39574da3bb27fbebdc2dca7ca9c3bc920d95a445
Sam Ireland <sam.ireland.uk@gmail.com>
2019-06-22 13:35:54 +0100

    Enhance setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.rst") as f:
+    long_description = f.read()
+
 setup(
  name="atomium",
  version="1.0.0",
  description="A molecular modeller and file parser.",
+ long_description=long_description,
+ long_description_content_type="text/x-rst",
  url="https://atomium.samireland.com",
  author="Sam Ireland",
  author_email="mail@samireland.com",
@@ -17,7 +22,8 @@ setup(
   "Programming Language :: Python :: 3.6",
   "Programming Language :: Python :: 3.7",
  ],
- keywords="chemistry bioinformatics proteins biochemistry molecules PDB XYZ",
+ keywords="chemistry bioinformatics proteins biochemistry molecules PDB MMCIF CIF MMTF",
  packages=["atomium"],
+ python_requires="!=2.*, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
  install_requires=["numpy", "requests", "rmsd", "paramiko", "msgpack", "valerius"]
 )

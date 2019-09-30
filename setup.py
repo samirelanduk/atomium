ded92d4b7301f9a2bb3b26fd132a6a8acef206a5
Sam Ireland <sam.ireland.uk@gmail.com>
2019-09-30 17:50:37 +0100

    Start v1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as f:
 
 setup(
  name="atomium",
- version="1.0.1",
+ version="1.0.2",
  description="A molecular modeller and file parser.",
  long_description=long_description,
  long_description_content_type="text/x-rst",

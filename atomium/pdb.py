077f181b95cd85813ff2ac09d67c95420c33d1fd
Sam Ireland <mail@samireland.com>
2022-04-21 22:32:21 +0100

    Redo HEADER parsing (fixes #35)

--- atomium/pdb.py
+++ atomium/pdb.py
@@ -1,4 +1,29 @@
+import re
+import calendar
 
-def pdb_string_to_mmcif_dict(bytestring):
+def pdb_string_to_mmcif_dict(filestring):
+    mmcif = {}
+    parse_header(filestring, mmcif)
+    return mmcif
 
-    return {}
+
+def parse_header(filestring, mmcif):
+    header = re.search(f"HEADER.+", filestring, re.M).group(0)
+    code = (header[62:66].strip() or "1XXX") if header else "1XXX"
+    if set(code) == {"-"}: code = "1XXX"
+    mmcif["entry"] = [{"id": code}]
+    date = header[50:59].strip()
+    day, month, year = date.split("-")
+    month = str(list(calendar.month_abbr).index(month.title())).zfill(2)
+    if len(year) == 2:
+        if int(year) > 50: year = "19" + year
+        if int(year) <= 50: year = "20" + year
+    mmcif["pdbx_database_status"] = [{
+        "status_code": "REL", "entry_id": "1LOL",
+        "recvd_initial_deposition_date": f"{year}-{month}-{day}"
+    }]
+    keyword = (header[10:50].strip() or "?") if header else "?"
+    if set(keyword) == {"-"} or keyword in ["NULL", "NONE"]: keyword = "?"
+    mmcif["struct_keywords"] = [{
+        "entry_id": code, "pdbx_keywords": keyword, "text": "?"
+    }]
--- tests/integration/test_pdb.py
+++ tests/integration/test_pdb.py
@@ -6,3 +6,12 @@ class DictParsingTests(TestCase):
 
     def test_1lol_pdb(self):
         d = atomium.open("tests/integration/files/1lol.pdb", dictionary=True)
+        
+        self.assertEqual(d["entry"], [{"id": "1LOL"}])
+        self.assertEqual(d["struct_keywords"], [
+            {"entry_id": "1LOL", "pdbx_keywords": "LYASE", "text": "?"}
+        ])
+        self.assertEqual(d["pdbx_database_status"], [{
+            "status_code": "REL", "entry_id": "1LOL",
+            "recvd_initial_deposition_date": "2002-05-06"
+        }])
